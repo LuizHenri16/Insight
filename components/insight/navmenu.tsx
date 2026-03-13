1f1f962d9fb24b3cbbf2fe888fd0d5b88864254e
LuizHenri16 <lbastos722@gmail.com>
2026-03-13 15:13:24 -0300

    feature: adicionar estrutura do menu de seleções

---
+++ components/insight/navmenu.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useState } from "react";
+import { BaseModal } from "./modal";
+import { CreateForm } from "./form/createForm";
+
+export const NavMenu = () => {
+    const [isModalAddOpen, setIsModalAddOpen] = useState(false);
+
+    const handleModalAddOpen = () => {
+        setIsModalAddOpen(!isModalAddOpen);
+    }
+
+    return (
+        <div className="bg-white border-1 border-gray-300 rounded-xl p-2 ">
+            <button onClick={handleModalAddOpen}><img src="/assets/icons/add.svg" alt="" />Adicionar</button>
+            {isModalAddOpen && (
+                <BaseModal isOpen={isModalAddOpen} onClose={handleModalAddOpen} title="Cadastrar Relacionamento">
+                    <CreateForm />
+                </BaseModal>
+            )}
+        </div>
+    )
+}
